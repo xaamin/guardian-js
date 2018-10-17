40afa53c0a7c7cfa57df922c9eb3b8cc82f596ec
Benjamín Martínez Mateos <xaamin@outlook.com>
2018-10-17 12:54:56 -0500

    Added new alias

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const GuardianService = require('./Guardian');
 const User = require('./Support/User');
 
 const Guardian = new GuardianService();
+const Gate = Guardian;
 
 module.exports = {
     Guardian,
+    Gate,
     User
 }
